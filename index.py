c31a0b58fb47812d21181a7dbbc5cc9f4bfbd24d
Sneha Inguva <snehainguva@Snehas-MacBook-Pro.local>
2016-03-09 21:46:54 -0500

    first time implementing this so trying out a bunch of ideas LOL turns out my way of testing concurrent connections w/ a tcp client was kinda stupid and i could just use twisted or locust.io

--- index.py
+++ index.py
@@ -1,42 +1,166 @@
-import socket
-import threading
-import time
+# import socket
+# import threading
+# import time
 
-#important variables
-TCP_IP = '127.0.01'
-TCP_PORT = 8080
-BUFFER_SIZE = 1024
+# #important variables
+# TCP_IP = '127.0.01'
+# TCP_PORT = 8080
+# BUFFER_SIZE = 1024
 
-class ClientThread(Thread):
+# #ATTEMPT 1- USING SOCKETS
+# import socket
+# from threading import Thread
+# from SocketServer import ThreadingMixIn
  
-    def __init__(self,ip,port):
-        Thread.__init__(self)
-        self.ip = ip
-        self.port = port
-        print "[+] New thread started for "+ip+":"+str(port)
+# class ClientThread(Thread):
+ 
+#     def __init__(self,ip,port):
+#         Thread.__init__(self)
+#         self.ip = ip
+#         self.port = port
+#         print "[+] New thread started for "+ip+":"+str(port)
  
  
-    def run(self):
-        while True:
-            data = conn.recv(2048)
-            if not data: break
-            print "received data:", data
-            conn.send(data)  # echo
+#     def run(self):
+#         while True:
+#             data = conn.recv(2048)
+#             if not data: break
+#             print "received data:", data
+#             conn.send(data)  # echo
+ 
+# TCP_IP = '0.0.0.0'
+# TCP_PORT = 62
+# BUFFER_SIZE = 20  # Normally 1024, but we want fast response
+ 
+ 
+# tcpsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# tcpsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+# tcpsock.bind((TCP_IP, TCP_PORT))
+# threads = []
+ 
+# while True:
+#     tcpsock.listen(4)
+#     print "Waiting for incoming connections..."
+#     (conn, (ip,port)) = tcpsock.accept()
+#     newthread = ClientThread(ip,port)
+#     newthread.start()
+#     threads.append(newthread)
+ 
+# for t in threads:
+#     t.join()
+
+# #ATTEMPT 2 -
+
+# #!/usr/bin/env python 
+
+# """ 
+# An echo server that uses threads to handle multiple clients at a time. 
+# Entering any line of input at the terminal will exit the server. 
+# """ 
+
+# import select 
+# import socket 
+# import sys 
+# import threading 
+
+# class Server: 
+#     def __init__(self): 
+#         self.host = '' 
+#         self.port = 50000 
+#         self.backlog = 5 
+#         self.size = 1024 
+#         self.server = None 
+#         self.threads = [] 
+
+#     def open_socket(self): 
+#         try: 
+#             self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+#             self.server.bind((self.host,self.port)) 
+#             self.server.listen(5) 
+#         except socket.error, (value,message): 
+#             if self.server: 
+#                 self.server.close() 
+#             print "Could not open socket: " + message 
+#             sys.exit(1) 
+
+#     def run(self): 
+#         self.open_socket() 
+#         input = [self.server,sys.stdin] 
+#         running = 1 
+#         while running: 
+#             inputready,outputready,exceptready = select.select(input,[],[]) 
+
+#             for s in inputready: 
+
+#                 if s == self.server: 
+#                     # handle the server socket 
+#                     c = Client(self.server.accept()) 
+#                     c.start() 
+#                     self.threads.append(c) 
+
+#                 elif s == sys.stdin: 
+#                     # handle standard input 
+#                     junk = sys.stdin.readline() 
+#                     running = 0 
+
+#         # close all threads 
+
+#         self.server.close() 
+#         for c in self.threads: 
+#             c.join() 
+
+# class Client(threading.Thread): 
+#     def __init__(self,(client,address)): 
+#         threading.Thread.__init__(self) 
+#         self.client = client 
+#         self.address = address 
+#         self.size = 1024 
+
+#     def run(self): 
+#         running = 1 
+#         while running: 
+#             data = self.client.recv(self.size) 
+#             if data: 
+#                 self.client.send(data) 
+#             else: 
+#                 self.client.close() 
+#                 running = 0 
+
+# if __name__ == "__main__": 
+#     s = Server() 
+#     s.run()
+
+#ATTEMPT 3 - 
+#APPROACH TO THIS
+import SocketServer
+
+class MyTCPHandler(SocketServer.BaseRequestHandler):
+    """
+    The request handler class for our server.
+
+    It is instantiated once per connection to the server, and must
+    override the handle() method to implement communication to the
+    client.
+    """
+
+    def handle(self):
+        # self.request is the TCP socket connected to the client
+        self.data = self.request.recv(1024).strip()
+        print "{} wrote:".format(self.client_address[0])
+        print self.data
+        # just send back the same data, but upper-cased
+        self.request.sendall(self.data.upper())
 
 if __name__ == "__main__":
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	s.bind((TCP_IP, TCP_PORT))
-	s.listen(1)
- 
-	conn, addr = s.accept()
-	print 'Connection address:', addr
-	
-	while 1:
-     	data = conn.recv(BUFFER_SIZE)
-     	if not data: break
-     	print "received data:", data
-     	conn.send(data)  # echo
-		conn.close()
+    HOST, PORT = "127.0.01", 8080
+
+    # Create the server, binding to localhost on port 9999
+    server = SocketServer.TCPServer((HOST, PORT), MyTCPHandler)
+
+    # Activate the server; this will keep running until you
+    # interrupt the program with Ctrl-C
+    server.serve_forever()
+
 
 #Ensure message sent is of the correct format using regex
 #<command>|<package>|<dependencies>
--- test_client.py
+++ test_client.py
@@ -1,15 +1,45 @@
+#Incredibly Simplified Method of Testing 
+#Grabbed a lot of this code from stack overflow and tweaked it
 import socket
+import time
+import threading
  
 TCP_IP = '127.0.0.1'
 TCP_PORT = 8080
 BUFFER_SIZE = 1024
-MESSAGE = "Hello, World!"
- 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((TCP_IP, TCP_PORT))
-s.send(MESSAGE)
-data = s.recv(BUFFER_SIZE)
-s.close()
 
-#
-print "received data:", data
+class ConnectThread(threading.Thread):
+
+    def connect(self):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        print('connecting to host')
+        sock.connect((TCP_IP,TCP_PORT))
+        return sock
+
+    def send(self, command):
+        sock = self.connect()
+        recv_data = ""
+        data = True
+
+        print('sending: ' + command)
+        sock.sendall(command)
+
+        while data:
+            data = sock.recv(BUFFER_SIZE)
+            recv_data += data 
+            print('received: ' + data)
+
+        sock.close()
+        return recv_data
+
+    def run(self,threadCount):
+    	count = 0
+    	for x in xrange(100):
+    		self.send(str(threadCount) + " STATUS"+str(count))
+    		time.sleep(2)
+    		count += 1
+
+if __name__ == "__main__":
+	for x in xrange(5):
+		ConnectThread().run(x)
+    
